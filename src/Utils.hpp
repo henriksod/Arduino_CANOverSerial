0abca713932d43ddf6288affb2bfb314190145f2
Henrik <henrik.a.soderlund@hotmail.com>
2023-07-05 20:22:38 +0200

    Fixed indent

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -40,22 +40,22 @@
 // against __clang__ here, since clang pretends to be GCC
 // 4.something and would otherwise be detected incorrectly here...
 #if !defined(__has_include) || defined(__GNUC__) && __GNUC__ < 5 && !defined(__clang__)
-   #if defined(ARDUINO_ARCH_ESP8266)
-      // ESP8266 does not have a working __has_include, but we
-      // know it does have a working libstdc++ with all the
-      // headers we care about, so provide a fake has_include
-      #define SERIALCAN_HAS_INCLUDE(x) 1
-   #elif defined(ARDUINO_SAM_DUE)
-      // Arduino DUE's GCC version is 4.8.3 (GCC < 5.0).
-      // If libstdc++ is used, std::function causes error
-      // so currently we disable libstdc++ and use our
-      // own implementations.
-      #define SERIALCAN_HAS_INCLUDE(x) 0
-   #else
-      #error "Compiler does not support __has_include, please report a bug against the SerialCAN library about this."
-   #endif
+    #if defined(ARDUINO_ARCH_ESP8266)
+        // ESP8266 does not have a working __has_include, but we
+        // know it does have a working libstdc++ with all the
+        // headers we care about, so provide a fake has_include
+        #define SERIALCAN_HAS_INCLUDE(x) 1
+    #elif defined(ARDUINO_SAM_DUE)
+        // Arduino DUE's GCC version is 4.8.3 (GCC < 5.0).
+        // If libstdc++ is used, std::function causes error
+        // so currently we disable libstdc++ and use our
+        // own implementations.
+        #define SERIALCAN_HAS_INCLUDE(x) 0
+    #else
+        #error "Compiler does not support __has_include, please report a bug against the SerialCAN library about this."
+    #endif
 #else
-   #define SERIALCAN_HAS_INCLUDE(x) __has_include(x)
+    #define SERIALCAN_HAS_INCLUDE(x) __has_include(x)
 #endif
 
 // Initializer_list *must* be defined in std, so take extra care to only
