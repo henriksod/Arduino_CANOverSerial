674378f11a5b97efba532a821323c5bc9d77400c
Henrik <henrik.a.soderlund@hotmail.com>
2023-07-05 19:59:29 +0200

    Fix linting

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -37,34 +37,35 @@ template<class _Ep>
 class initializer_list {
     const _Ep* __begin_;
     size_t    __size_;
-    
+
     inline
     constexpr
     initializer_list(const _Ep* __b, size_t __s) noexcept
         : __begin_(__b),
           __size_(__s)
     {}
-public:
+
+ public:
     typedef _Ep        value_type;
     typedef const _Ep& reference;
     typedef const _Ep& const_reference;
     typedef size_t    size_type;
-    
+
     typedef const _Ep* iterator;
     typedef const _Ep* const_iterator;
-    
+
     inline
     constexpr
     initializer_list() noexcept : __begin_(nullptr), __size_(0) {}
-    
+
     inline
     constexpr
     size_t    size()  const noexcept {return __size_;}
-    
+
     inline
     constexpr
     const _Ep* begin() const noexcept {return __begin_;}
-    
+
     inline
     constexpr
     const _Ep* end()   const noexcept {return __begin_ + __size_;}
